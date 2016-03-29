da5ad6e9bb7dead0add2e4f9ff512068fcbde6c8
David Lopez <dimor22@gmail.com>
2016-03-28 21:13:56 -0700

    testing email

--- testMailer.php
+++ testMailer.php
@@ -3,41 +3,30 @@ error_reporting(E_ALL);
 
 require 'vendor/phpmailer/phpmailer/class.phpmailer.php';
 
+//Create a new PHPMailer instance
 $mail = new PHPMailer;
-
-//var_dump($mail);
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'ecureserver.net';  // Specify main and backup SMTP servers
-//$mail->SMTPAuth = true;                               // Enable SMTP authentication
-//$mail->Username = 'info@novainteriorslv.com';                 // SMTP username
-//$mail->Password = 'Novainterior1';                           // SMTP password
-//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
-
-
-$mail->setFrom('tech@novainteriorslv.com', 'Mailer');
-$mail->addAddress('dimor22@gmail.com', 'David Lopez');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('tech@novainteriorslv.com', 'Support');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-
-////$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-////$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-//$mail->isHTML(true);                                  // Set email format to HTML
-//
-$mail->Subject = 'Here is the subject';
-//$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if($mail->send()) {
-	echo 'Message could not be sent.';
-	echo 'Mailer Error: ' . $mail->ErrorInfo;
+// Set PHPMailer to use the sendmail transport
+$mail->isSendmail();
+//Set who the message is to be sent from
+$mail->setFrom('tech@novainteriorslv.com', 'Nova Interiors');
+//Set an alternative reply-to address
+$mail->addReplyTo('tech@novainteriorslv.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('dimor22@gmail.com', 'David Lopez');
+//Set the subject line
+$mail->Subject = 'PHPMailer sendmail test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML('<p>thml content here <strong>bold</strong></p>');
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+	echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-	echo 'Message has been sent';
+	echo "Message sent!";
 }
 
 
