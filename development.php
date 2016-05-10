9b2c02a8df0f0b7f11c44427ceeed301211c61f7
David Lopez <dimor22@gmail.com>
2016-05-10 01:31:37 -0700

    Removed sensitive data files

--- development.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * PRODUCTION ENV VARIABLES
- */
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'slimdb1');
-define('DB_USER', 'root');
-define('DB_PWD', 'root');
--- production.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * PRODUCTION ENV VARIABLES
- */
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'slimdb1');
-define('DB_USER', 'nova_admin');
-define('DB_PWD', 'Green@p1e');
