a87a427aa969fab2c9f105ce936bd3ecec73a79f
David Lopez <dimor22@gmail.com>
2017-03-24 10:13:53 -0700

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,9 +4,9 @@
  * ORM CONFIG FILE
  */
 ORM::configure([
-	'connection_string' => 'mysql:host=localhost;dbname=slimdb1',
-	'username' => 'root',
-	'password' => 'root'
+	'connection_string' => 'mysql:host=yourHost;dbname=yourDB',
+	'username' => 'yourUserName',
+	'password' => 'yourPassWord'
 ]);
 ORM::configure('return_result_sets', true); // returns result sets
 ORM::configure('logging', true); // access with ORM::get_last_query() or ORM::get_query_log()
